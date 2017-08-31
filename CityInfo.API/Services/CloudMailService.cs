b586d3a1db8d16ae1ef03624cc23ce769b3280f9
Yusuf Demir <yusuf@bilgiedin.net>
2017-08-31 17:30:46 +0300

    appsettings.json config file ekledik. startup da bunu çağırdık . MailServisleri config dosyası üzerinden kullandık.

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -8,8 +8,8 @@ namespace CityInfo.API.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
+        private string _mailTo = Startup.Configuration["admin@mycompany.com"];
+        private string _mailFrom = Startup.Configuration["noreply@mycompany.com"];
 
         public void Send(string subject, string message)
         {
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
@@ -10,6 +11,18 @@ namespace CityInfo.API
 {
     public class Startup
     {
+
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional:false,reloadOnChange:true);
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 
